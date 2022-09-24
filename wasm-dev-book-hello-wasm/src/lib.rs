9715635b6f444181f5bf543c68f87f948c1bcf3c
yassun <yassun910@gmail.com>
2022-09-24 12:24:26 +0900

    hello wasm

---
+++ wasm-dev-book-hello-wasm/src/lib.rs
@@ -0,0 +1,29 @@
+extern {
+    fn date_now() -> f64;
+}
+extern crate tinymt;
+
+use tinymt::tinymt32;
+
+#[no_mangle]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[no_mangle]
+pub fn get_timestamp() -> f64 {
+    unsafe {
+        date_now()
+    }
+}
+
+#[no_mangle]
+pub fn rand() -> u32 {
+    let param = tinymt32::Param {
+        mat1: 0x8F7011EE,
+        mat2: 0xFC78FF1F,
+        tmat: 0x3793fdff,
+    };
+    let seed = 1;
+    tinymt32::from_seed(param, seed).gen()
+}
